3af5d0c9bfcaf950ef8ef3448fa9c2bd199aae41
Henry Barreto <me@henrybarreto.dev>
2023-09-23 18:54:37 -0300

    YyKJCq4vtnL8Qi4i1F5WYUQvWie+DKw/tjjZoHld/Ig=

--- examples/ip_in_udp/main.rs
+++ examples/ip_in_udp/main.rs
@@ -1,7 +1,10 @@
+use std::sync::mpsc::{self, channel, Receiver, Sender};
 use std::{
     fs::File,
     io::{Read, Write},
     net::{Ipv4Addr, SocketAddrV4, UdpSocket},
+    sync::{Arc, Mutex},
+    thread,
 };
 
 use packet::{Builder, Packet};
@@ -24,16 +27,116 @@ fn main() {
     file.read_to_string(&mut contents)
         .expect("Failed to read file");
 
-    let d: Data = toml::from_str(&contents).unwrap();
+    // let d: Data = toml::from_str(&contents).unwrap();
 
     let mut config = tun::Configuration::default();
     config
-        .address(Ipv4Addr::from(d.host))
+        .address(Ipv4Addr::new(10, 0, 0, 1))
         .netmask((255, 255, 255, 0))
         .up();
 
-    let mut dev = tun::create(&config).unwrap();
+    let dev = tun::create(&config).unwrap();
     let mut buffer = [0; 4096];
+
+    let socket = UdpSocket::bind("0.0.0.0:8080").expect("couldn't bind to address");
+    let address = format!("{}.{}.{}.{}:{}", 172, 17, 0, 2, 8080);
+    socket.connect(address).expect("connect function failed");
+
+    let mdev = Arc::new(Mutex::new(dev));
+    let msocket = Arc::new(socket);
+
+    let mut port_src = Arc::new(Mutex::new(0));
+    let mut port_dst = Arc::new(Mutex::new(0));
+
+    let cport_src = port_src.clone();
+    let cport_dst = port_dst.clone();
+    let csocket = msocket.clone();
+    let cdev = mdev.clone();
+    thread::spawn(move || loop {
+        // let mut dev = cdev.lock().unwrap();
+        // let read = dev.read(&mut buffer).unwrap();
+        // drop(dev);
+
+        if let Ok(ip) = packet::ip::v4::Packet::new(&buffer[..read]) {
+            match ip.protocol() {
+                packet::ip::Protocol::Udp => {
+                    let udp = packet::udp::Packet::new(ip.payload()).unwrap();
+
+                    println!("udp: {:?}", udp.source());
+                    println!("udp: {:?}", udp.destination());
+
+                    let mut port_src = cport_src.lock().unwrap();
+                    let mut port_dst = cport_dst.lock().unwrap();
+                    *port_src = udp.source();
+                    *port_dst = udp.destination();
+                    drop(port_src);
+                    drop(port_dst);
+
+                    let socket = &csocket;
+                    socket.send(&buffer[..read]).expect("couldn't send message");
+                    println!("WROTE TO SOCKET");
+                }
+                _ => {
+                    println!("PACKET TYPE NOT SUPPORTED");
+                }
+            }
+        } else {
+            println!("PACKET NOT IP");
+        }
+    });
+
+    let cport_src = port_src.clone();
+    let cport_dst = port_dst.clone();
+    let csocket = msocket.clone();
+    let cdev = mdev.clone();
+    thread::spawn(move || loop {
+        let socket = &csocket;
+
+        let mut buf = [0; 4096];
+        let read = socket.recv(&mut buf).expect("didn't receive data");
+        println!("read from socket {:?} bytes", &buf[..read]);
+
+        let port_src = cport_src.lock().unwrap();
+        let port_dst = cport_dst.lock().unwrap();
+        let port_src = *port_src;
+        let port_dst = *port_dst;
+        drop(port_src);
+        drop(port_dst);
+
+        println!("port_src: {:?}", port_src);
+        println!("port_dst: {:?}", port_dst);
+
+        println!("CREATING PACKET");
+        let response = packet::ip::v4::Builder::default()
+            .id(0x01)
+            .unwrap()
+            .ttl(64)
+            .unwrap()
+            .source(Ipv4Addr::new(10, 0, 0, 100))
+            .unwrap()
+            .destination(Ipv4Addr::new(10, 0, 0, 1))
+            .unwrap()
+            .udp()
+            .unwrap()
+            .source(port_dst)
+            .unwrap()
+            .destination(port_src)
+            .unwrap()
+            .payload(&buf[..read])
+            .unwrap()
+            .build()
+            .unwrap();
+
+        // let mut dev = cdev.lock().unwrap();
+        // println!("WRITING TO DEV");
+        // dev.write(&response).unwrap();
+        // println!("WROTE TO DEV");
+        // drop(dev);
+    });
+
+    loop {
+        thread::sleep(std::time::Duration::from_secs(1));
+    }
 
     /*let mut socket = socket2::Socket::new_raw(
         socket2::Domain::IPV4,
@@ -46,7 +149,7 @@ fn main() {
     socket.set_header_included(true).unwrap();
     socket.connect(&address).unwrap();*/
 
-    let socket = UdpSocket::bind("0.0.0.0:8080").expect("couldn't bind to address");
+    /*let socket = UdpSocket::bind("0.0.0.0:8080").expect("couldn't bind to address");
     let address = format!(
         "{}.{}.{}.{}:{}",
         d.server[0], d.server[1], d.server[2], d.server[3], 8080
@@ -183,5 +286,5 @@ fn main() {
                 }
             }
         }
-    }
+    }*/
 }
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // First phase:
+    //
+    // 1. creates the tun device: 10.0.0.100
+    // 2. creates the udp socket:
+    // 3. creates thread to read from tun and write to udp
+    // 4. creates thread to read from udp and write to tun
+}
