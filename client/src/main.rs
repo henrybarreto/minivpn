226ba4147cf75468780225f156e1f73978504b27
Henry Barreto <me@henrybarreto.dev>
2023-10-29 00:27:06 -0300

    6y5aerGvGsGsgL8ffNgTxvyCEicI3uANhmeNZRQ7WpM=

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,164 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    io::{Read, Write},
+    net::{IpAddr, Ipv4Addr, SocketAddrV4, TcpStream, UdpSocket},
+    str::FromStr,
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use bincode;
+use packet::{Builder, Packet};
+use serde::{Deserialize, Serialize};
+
+extern crate tun;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Identity {
+    pub network: u64,
 }
+
+fn main() {
+    let mut config = tun::Configuration::default();
+    let args: Vec<String> = env::args().collect();
+
+    println!("args: {:?}", args);
+    config
+        // .address(Ipv4Addr::new(10, 0, 0, 100))
+        .address(Ipv4Addr::from_str(&args[1]).unwrap())
+        .netmask((255, 255, 255, 0))
+        .up();
+
+    let dev = tun::create(&config).unwrap();
+    dev.set_nonblock().unwrap();
+
+    let socket = UdpSocket::bind("0.0.0.0:8080").expect("couldn't bind to address");
+    let address = &args[2];
+    socket.connect(address).expect("connect function failed");
+
+    let mdev = Arc::new(Mutex::new(dev));
+    let msocket = Arc::new(socket);
+
+    let mut csocket = msocket.clone();
+    let cdev = mdev.clone();
+    thread::spawn(move || loop {
+        let mut buffer = [0; 4096];
+
+        let mut dev = cdev.lock().unwrap();
+        let read: usize = match dev.read(&mut buffer) {
+            Ok(read) => read,
+            Err(_) => {
+                continue;
+            }
+        };
+        drop(dev);
+
+        if let Ok(ip) = packet::ip::v4::Packet::new(&buffer[..read]) {
+            println!("PACKET IS IP");
+
+            let socket = &mut csocket;
+            socket.send(&buffer[..read]).expect("couldn't send message");
+            println!("WROTE TO SOCKET");
+        } else {
+            println!("PACKET NOT IP");
+        }
+    });
+
+    let csocket = msocket.clone();
+    let cdev = mdev.clone();
+    thread::spawn(move || loop {
+        let socket = &csocket;
+
+        let mut buffer = [0; 4096];
+        dbg!("B");
+        let read = match socket.recv(&mut buffer) {
+            Ok(read) => {
+                dbg!("D");
+
+                read
+            }
+            Err(_) => {
+                continue;
+            }
+        };
+        dbg!("C");
+
+        if let Ok(ip) = packet::ip::v4::Packet::new(&buffer[..read]) {
+            println!("PACKET IS IP");
+
+            let mut dev = cdev.lock().unwrap();
+            match dev.write(&buffer[..read]) {
+                Ok(usize) => {
+                    println!("WROTE TO DEV");
+                    println!("WROTE {:?} BYTES", usize);
+                    dbg!(ip);
+                }
+                Err(e) => {
+                    println!("ERROR WRITING TO DEV {:?}", e);
+                    continue;
+                }
+            }
+            drop(dev);
+        } else {
+            println!("PACKET NOT IP");
+        }
+    });
+
+    loop {
+        thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
+
+// let mut dev = cdev.lock().unwrap();
+// println!("WRITING TO DEV");
+// match dev.write_all(&response) {
+//     Ok(_) => {}
+//     Err(_) => {
+//         continue;
+//     }
+// }
+// println!("WROTE TO DEV");
+//drop(dev);
+// match ip.protocol() {
+//     packet::ip::Protocol::Udp => {
+//         let udp = packet::udp::Packet::new(ip.payload()).unwrap();
+
+//         println!("CREATING PACKET");
+//         let response = packet::ip::v4::Builder::default()
+//             .id(0x01)
+//             .unwrap()
+//             .ttl(64)
+//             .unwrap()
+//             .source(ip.source())
+//             .unwrap()
+//             .destination(ip.destination())
+//             .unwrap()
+//             .udp()
+//             .unwrap()
+//             .source(udp.source())
+//             .unwrap()
+//             .destination(udp.destination())
+//             .unwrap()
+//             .payload(udp.payload())
+//             .unwrap()
+//             .build()
+//             .unwrap();
+
+//         let mut dev = cdev.lock().unwrap();
+//         println!("WRITING TO DEV");
+//         match dev.write_all(&response) {
+//             Ok(_) => {}
+//             Err(_) => {
+//                 continue;
+//             }
+//         }
+//         println!("WROTE TO DEV");
+//         drop(dev);
+//     }
+//     packet::ip::Protocol::Tcp => {
+//         println!("PACKET TYPE TCP");
+//     }
+//     _ => {
+//         println!("PACKET TYPE NOT SUPPORTED");
+//     }
+// }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,101 @@
+use packet::Packet;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{Read, Write},
+    net::{IpAddr, Ipv4Addr, SocketAddrV4, TcpListener, UdpSocket},
+    string,
+    sync::{Arc, Mutex},
+    thread,
+};
+
+//if let None = origin {
+//    println!("key {:?}", ip.source());
+//    println!("value {:?}", addr.ip());
+
+//    networks.insert(ip.source().into(), addr.ip());
+
+//    println!("inserted key {:?} and {:?}", addr.ip(), ip.source());
+
+//    if let None = peer {
+//        csocket.send_to(&buffer, peer.unwrap().to_string()).unwrap();
+//    }
+//} else {
+//    let mut value = peer.unwrap().clone().to_string();
+//    println!(
+//        "already inseted the key {:?} with value {:?}",
+//        ip.source(),
+//        value
+//    );
+
+//    value.push_str(":8080");
+
+//    println!("with port {:?}", value);
+
+//    csocket.send_to(&buffer, value).unwrap();
+//}
+
 fn main() {
-    println!("Hello, world!");
+    let mut networks = HashMap::<IpAddr, IpAddr>::new();
+    let mnetworks = Arc::new(Mutex::new(networks));
+
+    let socket = UdpSocket::bind("0.0.0.0:8081").unwrap();
+
+    let msocket = Arc::new(socket);
+
+    let cnetworks = mnetworks.clone();
+    let csocket = msocket.clone();
+    thread::spawn(move || loop {
+        let mut buffer = [0; 4096];
+
+        let (len, addr) = csocket.recv_from(&mut buffer).unwrap();
+        println!("{:?} bytes received from {:?}", len, addr);
+
+        if let Ok(ip) = packet::ip::v4::Packet::new(&buffer) {
+            println!("PACKET IS IP");
+
+            let mut n = cnetworks.lock().unwrap();
+
+            let origin = n.get(&ip.source().into());
+
+            if let None = origin {
+                println!("key {:?}", ip.source());
+                println!("value {:?}", addr.ip());
+
+                n.insert(ip.source().into(), addr.ip());
+
+                println!("inserted key {:?} and {:?}", addr.ip(), ip.source());
+            }
+
+            let peer = n.get(&ip.destination().into());
+
+            if let None = peer {
+                println!("peer is none");
+            } else {
+                let mut value = peer.unwrap().clone().to_string();
+                println!(
+                    "already inseted the key {:?} with value {:?}",
+                    ip.source(),
+                    value
+                );
+
+                value.push_str(":8080");
+
+                println!("with port {:?}", value);
+
+                let ip = packet::ip::v4::Packet::new(&buffer).unwrap();
+                dbg!(ip);
+                println!("sending to {:?}", value);
+                println!("sending to {:?}", ip.destination());
+
+                csocket.send_to(&buffer, value).unwrap();
+            }
+        } else {
+            println!("PACKET NOT IP");
+        }
+    });
+
+    loop {
+        thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
