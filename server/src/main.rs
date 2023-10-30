333d79395fc38f5c814061de5f91b9477e4b73b0
Henry Barreto <me@henrybarreto.dev>
2023-10-30 08:03:05 -0300

    XM0vCfIQGmBhi6jfOKtFZXegmUBUewaW1zfDlx29VuA=

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,19 @@
 use std::{
+    cell::RefCell,
     collections::HashMap,
     io::{Read, Write},
     net::{IpAddr, TcpListener, TcpStream},
-    sync::{Arc, Mutex},
+    sync::{
+        mpsc::{self, Sender},
+        Arc,
+    },
     thread,
 };
 
-fn handle(mut socket: TcpStream, cnetworks: Arc<Mutex<HashMap<IpAddr, TcpStream>>>) {
+fn handle(mut socket: TcpStream, sender: Arc<Sender<(TcpStream, Vec<u8>)>>) {
     thread::spawn(move || loop {
-        let mut buffer = [0; 4096];
+        // let mut buffer = [0; 4096];
+        let mut buffer = Vec::new();
 
         let _ = match socket.read(&mut buffer) {
             Ok(read) => {
@@ -21,43 +26,56 @@ fn handle(mut socket: TcpStream, cnetworks: Arc<Mutex<HashMap<IpAddr, TcpStream>
             Err(_) => break,
         };
 
-        if let Ok(ip) = packet::ip::v4::Packet::new(&buffer) {
-            let source: IpAddr = ip.source().into();
-            let destination: IpAddr = ip.destination().into();
-
-            let mut networks = cnetworks.lock().unwrap();
-            dbg!(&networks);
-
-            networks.insert(source, socket.try_clone().unwrap());
-
-            if let Some(to) = networks.get(&destination) {
-                match to.try_clone() {
-                    Ok(mut dest) => {
-                        dest.write(&buffer).unwrap();
-                    }
-                    Err(_) => {
-                        networks.remove(&destination);
-                    }
-                }
-
-                drop(networks);
-            }
-        } else {
-            dbg!("PACKET NOT IP");
-        }
+        sender.send((socket.try_clone().unwrap(), buffer)).unwrap();
     });
 }
 
 fn main() {
-    let networks = HashMap::<IpAddr, TcpStream>::new();
-    let mnetworks = Arc::new(Mutex::new(networks));
-
     let listener = TcpListener::bind("0.0.0.0:8081").unwrap();
 
+    let (tx, rx) = mpsc::channel::<(TcpStream, Vec<u8>)>();
+    let atx = Arc::new(tx);
+
+    thread::spawn(move || {
+        let networks = HashMap::<IpAddr, TcpStream>::new();
+        let rnetworks = RefCell::new(networks);
+
+        for (socket, buffer) in rx {
+            // dbg!(buffer);
+
+            if let Ok(ip) = packet::ip::v4::Packet::new(&buffer) {
+                let source: IpAddr = ip.source().into();
+                let destination: IpAddr = ip.destination().into();
+
+                dbg!(&rnetworks);
+
+                let mut cnetworks = rnetworks.borrow_mut();
+
+                cnetworks.insert(source, socket.try_clone().unwrap());
+
+                if let Some(to) = cnetworks.get(&destination) {
+                    match to.try_clone() {
+                        Ok(mut dest) => {
+                            dest.write(&buffer).unwrap();
+                        }
+                        Err(_) => {
+                            cnetworks.remove(&destination);
+                        }
+                    }
+                }
+
+                drop(cnetworks);
+            } else {
+                dbg!("PACKET NOT IP");
+            }
+        }
+    });
+
     for stream in listener.incoming() {
-        let cnetworks = mnetworks.clone();
+        let catx = atx.clone();
+
         match stream {
-            Ok(stream) => handle(stream.try_clone().unwrap(), cnetworks),
+            Ok(stream) => handle(stream.try_clone().unwrap(), catx),
             Err(e) => {
                 dbg!(e);
 
